c1a09fdefd4ada0ee2dde91e62eb59f42c0d15be
Thiago Jacinto <46906069+thiagojacinto@users.noreply.github.com>
2020-07-29 00:32:15 -0300

    [component] topbar fixes

--- src/components/Topbar/index.js
+++ src/components/Topbar/index.js
@@ -1,10 +1,11 @@
 import React from "react";
-import styled from "styled-components";
+import styled, { ThemeProvider } from "styled-components";
 
 import LogoImage from "../../assets/Logo.png";
+import Button from "../Button";
 
-export const Bar = styled.div`
-  width: 100%;
+export const Bar = styled.header`
+  width: 100vw;
   height: 20%;
   padding: 1em 0;
 
@@ -14,16 +15,34 @@ export const Bar = styled.div`
 
   transition: 0.3s;
 
-  & .logo {
+  .logo {
+    height: 100%;
     width: auto;
-    height: 80%;
+  }
+
+  @media screen and (max-width: 600px) {
+    & .logo {
+      height: var(--LOGO_HEIGHT);
+    }
+  }
+
+  @media screen and (max-height: 768px) {
+    height: var(--LOGO_HEIGHT);
   }
 `;
+
+const theme = {
+  color: "var(--primary_color)",
+};
 
 export const Topbar = () => {
   return (
     <Bar className="container">
-      <img className="logo" src={LogoImage} alt="Devflix logo" />
+      <img className="logo" src={LogoImage} alt="Devflix Logo" />
+
+      <ThemeProvider theme={theme}>
+        <Button>Add movie</Button>
+      </ThemeProvider>
     </Bar>
   );
 };
