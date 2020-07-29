2dfec5aa5ec97f7990606aab1bd46a48c13688e0
Thiago Jacinto <46906069+thiagojacinto@users.noreply.github.com>
2020-07-29 00:32:39 -0300

    [new] Topbar + Footer implemented

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
-import "./App.css";
 import Topbar from "./components/Topbar";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <>
       <Topbar />
+
+      <Footer />
     </>
   );
 }
