784a4b23a2e81e767db4773ef90417103c58ad10
Thiago Jacinto <46906069+thiagojacinto@users.noreply.github.com>
2020-07-29 00:31:46 -0300

    [component] button created

---
+++ src/components/Button/index.js
@@ -0,0 +1,32 @@
+import styled from "styled-components";
+import "../../styles/onHoverEffect.css";
+
+export const Button = styled.button`
+  font-size: 1em;
+  padding: 0.25em 1em;
+  border-radius: 5px;
+
+  cursor: pointer;
+  font-weight: bold;
+
+  transition: opacity 0.3s;
+  &:hover,
+  &:focus {
+    opacity: 0.7;
+    background-color: ${(props) => props.theme.bg};
+    color: ${(props) => props.theme.dark};
+  }
+
+  color: ${(props) => props.theme.color};
+  border: 2px solid ${(props) => props.theme.color};
+`;
+
+Button.defaultProps = {
+  theme: {
+    color: "var(--primary_color)",
+    bg: "var(--primary_bg)",
+    dark: "var(--dark)",
+  },
+};
+
+export default Button;
