114c8b2f02836609a3fe0b8360e08583a2297959
Thiago Jacinto <46906069+thiagojacinto@users.noreply.github.com>
2020-07-30 16:08:06 -0300

    [test] fixes to handle Router implem.

--- src/__test__/Topbar.test.js
+++ src/__test__/Topbar.test.js
@@ -2,17 +2,18 @@ import React from "react";
 import { render } from "@testing-library/react";
 
 import Topbar from "../components/Topbar";
+import { MemoryRouter } from "react-router-dom";
 
 describe("Testing Topbar", () => {
   test("Rendering a Logo", () => {
-    const bar = render(<Topbar />);
+    const bar = render(<Topbar />, { wrapper: MemoryRouter });
     const result = bar.getByRole("img");
 
     expect(result).toBeDefined();
   });
 
   test("Rendering a add Button", () => {
-    const bar = render(<Topbar />);
+    const bar = render(<Topbar />, { wrapper: MemoryRouter });
     const result = bar.getByRole("button", "/Add movie/i");
 
     expect(result).toBeDefined();
