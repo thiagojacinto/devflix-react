f96806e85cc1abe3c64bc26db12937ec0683736c
Thiago Jacinto <46906069+thiagojacinto@users.noreply.github.com>
2020-08-02 23:37:26 -0300

    [new] global themeProvider + `addMovie` route

--- src/App.js
+++ src/App.js
@@ -1,28 +1,35 @@
 import React from "react";
 import { Switch, Route, BrowserRouter } from "react-router-dom";
+import { ThemeProvider } from "styled-components";
+
 import PageNotFound from "./pages/PageNotFound";
 import Home from "./pages/Home";
+import AddMovie from "./pages/AddMovie";
 
-function App() {
+const defaultTheme = {
+  color: "var(--primary_color)",
+  bg: "var(--primary_bg)",
+  dark: "var(--dark)",
+};
+
+function App({ theme }) {
   return (
     <BrowserRouter>
-      <Switch>
-        <Route path="/" exact>
-          <Home />
-        </Route>
+      <ThemeProvider theme={theme ? theme : defaultTheme}>
+        <Switch>
+          <Route path="/" exact>
+            <Home />
+          </Route>
 
-        {/* <Route path="/addmovie">
-          <AddMovieForm />
-        </Route> */}
+          <Route path="/addmovie">
+            <AddMovie />
+          </Route>
 
-        {/* <Route path="/video">
-          <VideoPlayer />
-        </Route> */}
-
-        <Route path="*">
-          <PageNotFound />
-        </Route>
-      </Switch>
+          <Route path="*">
+            <PageNotFound />
+          </Route>
+        </Switch>
+      </ThemeProvider>
     </BrowserRouter>
   );
 }
